1e14c7d827e0e9efe085da3cbafac1ea198790c4
Araqelyan Tiran <tirans3777@gmail.com>
2019-04-01 16:12:50 +0400

    Update class Alphabet

--- Project_ArmenianLetter/Project_ArmenianLetter/Alphabet.cs
+++ Project_ArmenianLetter/Project_ArmenianLetter/Alphabet.cs
@@ -8,5 +8,22 @@ namespace Project_ArmenianLetter
 {
     class Alphabet
     {
+        enum Latin
+        {
+            A,B,C,D,E,F,G,
+            H,I,J,K,L,M,N,
+            O,P,Q,R,S,T,U,
+            V,W,X,Z,Y,
+        }
+
+        enum Armenian
+        {
+            Ա,Բ,Գ,Դ,Ե,Զ,Է,
+            Ը,Թ,Ժ,Ի,Լ,Խ,Ծ,
+            Կ,Հ,Ձ,Ղ,Ճ,Մ,Յ,
+            Ն,Շ,Ո,Չ,Պ,Ջ,Ռ,
+            Ս,Վ,Տ,Ր,Ց,ՈՒ,
+            Փ,Ք,ԵՎ,Օ,Ֆ
+        }
     }
 }
